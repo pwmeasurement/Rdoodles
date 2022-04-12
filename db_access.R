814b60fe94ee2f15ac0a2ca8355611417b3dd6ff
Phil Weissman <phil@pwmeasurement.com>
2022-04-12 17:33:51 -0400

    cleaned up the db files

--- db_access.R
+++ db_access.R
@@ -2,10 +2,6 @@
 # Walker Advertising data read
 # Nov 17, 2020
 
-### Mapping files used by WalkerDBProcess
-# walker_area_ca_maping
-# weight_wag_mapping
-# dim_stn_tz_mapping
 
 
 rm(list=ls()) 
@@ -21,21 +17,7 @@ library(tidyr)
 #library(PWMutils)
 
 
-
-#source("/Users/phil/source/utils.R")
-#source("/Users/phil/source/daypart2.R")
-#source("r/walker_utils.R")
-
-if(Sys.getenv("HOME") == "/Users/phil") {
-  bucket_hist = "rbase-docker/Reports_Hist/"
-  bucket_cur = "rbase-docker/Reports_Current/"
-  host <- "127.0.0.1"
-} else {
-  bucket_hist = "walker-s3-bucket/Reports_Hist"
-  bucket_cur = "walker-s3-bucket/Reports_Current"
-  host <- "walkerrdsmed-cluster.cluster-ro-ciucgnzjkyct.us-east-1.rds.amazonaws.com"
-}
-
+host <- "127.0.0.1"
 
 
 mydb <<- dbConnect(RMySQL::MySQL(), user='admin', password='Admin1234', dbname='walker', host=host)
